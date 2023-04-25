9bd8cf84ff4bca30644e90a166308136f2e52d14
Iulian COJOCARI <iuliancojocari42@gmail.com>
2023-04-26 00:14:48 +0200

    bug fixes : migrations, tests

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -5,8 +5,9 @@ from sentry_sdk.integrations.django import DjangoIntegration
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
 env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
