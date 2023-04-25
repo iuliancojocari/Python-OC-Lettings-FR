6bcdbec2fd29cc7c0fc2bc6a426e5ecbc515fbaa
Iulian COJOCARI <iuliancojocari42@gmail.com>
2023-04-26 01:07:00 +0200

    Test

--- oc_lettings_site/views.py
+++ oc_lettings_site/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 """
-Test Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+Lorem ipsum dolor sit amet, consectetur adipiscing elit.
 Quisque molestie quam lobortis leo consectetur ullamcorper non id est.
 Praesent dictum, nulla eget feugiat sagittis, sem mi convallis eros,
 vitae dapibus nisi lorem dapibus sem. Maecenas pharetra purus ipsum,
