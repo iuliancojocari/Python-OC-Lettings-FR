5e0bd0c62cae4eff95282177a4dfa68038c9deed
Iulian COJOCARI <iuliancojocari42@gmail.com>
2023-04-25 22:34:59 +0200

    bug fixes : migrations, tests

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
 
+
+def trigger_error(request):
+    division_by_zero = 1 / 0
+    return division_by_zero
+
+
 urlpatterns = [
-    path('', include('home.urls')),
-    path('', include('lettings.urls'), name='lettings'),
-    path('', include('profiles.urls'), name='profiles'),
     path('admin/', admin.site.urls),
+    path('', include('profiles.urls')),
+    path('', include('lettings.urls')),
+    path('', include('home.urls')),
+    path('sentry-debug/', trigger_error),
 ]
