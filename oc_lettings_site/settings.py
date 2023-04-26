f3301e9e5ed6d8f86ac75654de5fc785c9395815
Iulian COJOCARI <iuliancojocari42@gmail.com>
2023-04-26 13:36:25 +0200

    Dockerfile update

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -16,7 +16,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s'
+SECRET_KEY = env('SECRET_KEY', default=get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
